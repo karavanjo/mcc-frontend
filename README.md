9fff4d29fd531b83f4a5a8b2af31413e2184f46b
Ivan Kavaliou <info@ikovalev.com>
2022-01-12 01:07:29 +0300

    feat: add info modal; add mui/material lib

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# Frontend for web application "My city's climate"
+# "My city's climate" - historical weather data viewer
+
+"My city's climate" is a free online viewer for historical weather data.
 
 The application requires a climate database created by [mcc-import](https://github.com/karavanjo/mcc-import) application.
 
--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,12 +1,47 @@
-import logo from '../../logo.svg';
+import { Fragment, useState } from 'react';
 
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import Modal from '@mui/material/Modal'
+import Button from '@mui/material/Button'
+
+import logo from '../../logo.svg'
 import './Logo.scss'
 
 function Logo() {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
-    <div className="logo" title="My city's climate">
-      <img src={logo} alt="My city's climate"/>
-    </div>
+    <Fragment>
+      <div className="logo" title="My city's climate" onClick={handleOpen}>
+        <img src={logo} alt="My city's climate"/>
+      </div>
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+      >
+        <Box className="modal-logo-description">
+          <Typography variant="h6" component="h2">
+            My city's climate
+          </Typography>
+          <Typography sx={{ mt: 2 }}>
+            Free online viewer for historical weather data
+          </Typography>
+          <Typography sx={{ mt: 2 }}>
+            The application uses <a href="https://danepubliczne.imgw.pl/">Dane publiczne IMGW-PIB</a>
+          </Typography>
+          <Typography sx={{ mt: 2 }}>
+            Source code - <a href="https://github.com/karavanjo/mcc-frontend" title="karavanjo/mcc-frontend">github</a>
+          </Typography>
+          <Typography align={'right'} sx={{ mt: 2 }}>
+            <Button onClick={handleClose} variant="outlined">Close</Button>
+          </Typography>
+        </Box>
+      </Modal>
+    </Fragment>
   )
 }
 
